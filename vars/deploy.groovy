62c7ccfffb82681b77b7964f844d967722f81219
Elghazal Ahmed <geniousphp@gmail.com>
2017-12-14 14:28:59 +0000

    set inventory in deployment lib

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,3 @@
 def call(String env, String serviceName) {
-  sh "ansible-playbook deployment.pb.yml -e env_deployment=${env} -e serviceName=${serviceName}"
+  sh "ansible-playbook deployment.pb.yml -i inventories/deployment/inventory -e env_deployment=${env} -e serviceName=${serviceName}"
 }
